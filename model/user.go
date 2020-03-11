25cc953e696cafe809f15dd0af269b9696134d9b
Bohdan Volykh <wbodjaw@gmail.com>
2020-03-11 16:49:35 +0200

    add comments for User struct

--- model/user.go
+++ model/user.go
@@ -4,14 +4,24 @@ import (
 	"time"
 )
 
+// Struct with all necessary information about user
 type User struct {
-	ID        string    `json:"id"`         // Unique identifier
-	Username  string    `json:"user_name"`  // Unique login
-	Password  string    `json:"password"`   // Strong enough password
-	Email     string    `json:"email"`      // Valid email
-	FirstName string    `json:"first_name"` // Obviously first name
-	LastName  string    `json:"last_name"`  // Obviously last name
-	Phone     int       `json:"phone"`      // Valid phone
-	CreatedAt time.Time `json:"created_at"` // Time when user was created
-	UpdatedAt time.Time `json:"updated_at"` // Time of last changes made
+	// Unique identifier
+	ID string `json:"id"`
+	// Unique login
+	Username string `json:"user_name"`
+	// Strong enough password
+	Password string `json:"password"`
+	// Valid email
+	Email string `json:"email"`
+	// Obviously first name
+	FirstName string `json:"first_name"`
+	// Obviously last name
+	LastName string `json:"last_name"`
+	// Valid phone
+	Phone int `json:"phone"`
+	// Time when user was created
+	CreatedAt *time.Time `json:"created_at"`
+	// Time of last changes made
+	UpdatedAt *time.Time `json:"updated_at"`
 }
