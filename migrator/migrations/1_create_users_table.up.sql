59dea24668b517e936ae0c9202c85e13544c120d
Ostap Klymchuk <oklymchuk@gmail.com>
2020-03-16 12:52:08 +0200

    Fix with database text column length

--- migrator/migrations/1_create_users_table.up.sql
+++ migrator/migrations/1_create_users_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE public.users
 (
     id uuid NOT NULL,
-    user_name text NOT NULL,
-    "password" text NOT NULL,
-    email text NOT NULL,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    phone text NOT NULL,
+    user_name varchar(20) NOT NULL,
+    "password" varchar(20) NOT NULL,
+    email varchar(70) NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    phone varchar(12) NOT NULL,
     created_at timestamp NOT NULL,
     updated_at timestamp NULL,
     CONSTRAINT users_pk PRIMARY KEY (id)
