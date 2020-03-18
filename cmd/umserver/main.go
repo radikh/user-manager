eccf7a86684c069ff2f74302f4549c319a4e6f0f
Bohdan Volykh <wbodjaw@gmail.com>
2020-03-18 19:04:42 +0200

    move GracefulShutdown func into separate file

--- cmd/umserver/main.go
+++ cmd/umserver/main.go
@@ -6,17 +6,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
 )
-
-const GracefulShutdownTimeOut = 10 * time.Second
 
 func main() {
 	var (
@@ -29,7 +25,6 @@ func main() {
 
 	// TODO: Replace with HTTP server implemented in server package
 	srv := &http.Server{Addr: ":8099"}
-
 	// Go routine with run Server
 	wg.Add(1)
 	go func() {
@@ -41,46 +36,12 @@ func main() {
 			log.Printf("%v\n", err)
 		}
 	}()
-	log.Print("Server Listening...")
+	log.Printf("Server Listening at%s...", srv.Addr)
 
-	select {
-	case <-interrupt:
-		log.Print("Pressed Ctrl+C to terminate server...")
-		cancel()
-	case <-ctx.Done():
-		log.Print(ctx.Err())
-	}
-
-	log.Print("Server is Stopping...")
-
-	ctx, cancel = context.WithTimeout(context.Background(), GracefulShutdownTimeOut)
-	defer cancel()
-
-	err := srv.Shutdown(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if waitTimeout(ctx, wg) {
-		fmt.Println("Timed out waiting for wait group")
-	} else {
-		fmt.Println("Wait group finished")
-	}
-	log.Println("Server was successful stopped!")
-}
-
-// waitTimeout waits for the waitgroup for the specified max timeout.
-// Returns true if waiting timed out.
-func waitTimeout(ctx context.Context, wg *sync.WaitGroup) bool {
-	c := make(chan struct{})
 	go func() {
-		defer close(c)
-		wg.Wait()
+		GracefulShutdown(ctx, cancel, interrupt, srv)
 	}()
-	select {
-	case <-c:
-		return false // completed normally
-	case <-ctx.Done():
-		return true // timed out
-	}
+
+	WaitTimeout(ctx, wg)
+	log.Println("Server was successful stopped!")
 }
