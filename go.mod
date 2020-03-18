4264b988e32ac8fd49ccc61baf8299f7e1bcb873
Ostap Klymchuk <oklymchuk@gmail.com>
2020-03-18 20:05:10 +0200

    Add init folder logger and umlogger.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lvl484/user-manager
 
 go 1.13
+
+require github.com/sirupsen/logrus v1.4.2 // indirect
---
+++ logger/umlogger.go
@@ -0,0 +1,3 @@
+// Package logger provides implementetion of writing Log messages to Graylog, file, and stdout.
+// Supports log levels and destination.
+package logger
