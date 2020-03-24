3562d8ff6a6b7d941fe8d30ac5d651c44596cb2f
Vasyl Kosiv <vasylkosiv@gmail.com>
2020-03-24 16:44:50 +0200

    Add ConnectToDb

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lvl484/user-manager
 
 go 1.13
+
+require github.com/lib/pq v1.3.0
--- storage/postgres.go
+++ storage/postgres.go
@@ -1,7 +1,45 @@
-// Package storage provides implementation for persisting user information.
-// It includes the following methods:
-// - create new user
-// - get information about exists user
-// - update exists user
-// - delete unnecessary user
+// Package storage has to provide an interface to create a new postgres client
+// with ability to reconnect after connection failure.
 package storage
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type PgClient struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DbName   string
+	Db       *sql.DB
+}
+
+const pgStr = "host=%v port=%v user=%v password=%v dbname=%v sslmode=disable"
+
+func ConnectToDb(pg *PgClient) *sql.DB {
+
+	pgConfig := fmt.Sprintf(pgStr, pg.Host, pg.Port, pg.User, pg.Password, pg.DbName)
+
+	database, err := sql.Open("postgres", pgConfig)
+	if err != nil {
+		log.Print(err)
+		log.Print("Could not connect to ", pg.DbName)
+	}
+	if pg.IsAlive() != nil {
+		log.Print(err)
+		log.Print("Could not connect to ", pg.DbName)
+	}
+	log.Print("Successfully connected to ", pg.DbName)
+	pg.Db = database
+
+	return pg.Db
+}
+func (pg *PgClient) IsAlive() error {
+	err := pg.Db.Ping()
+	return err
+}
