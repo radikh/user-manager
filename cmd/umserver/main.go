db0b57e3efb43dc2d369f322815866a320ae4eae
Vasyl Kosiv <vasylkosiv@gmail.com>
2020-03-31 12:37:23 +0300

    Fix error

--- cmd/umserver/main.go
+++ cmd/umserver/main.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -16,33 +15,14 @@ import (
 	"syscall"
 	"time"
 
-	_ "github.com/lib/pq"
 	"github.com/lvl484/user-manager/config"
 	"github.com/lvl484/user-manager/logger"
 	"github.com/lvl484/user-manager/storage"
-	"github.com/sirupsen/logrus"
+
+	_ "github.com/lib/pq"
 )
 
 const gracefulShutdownTimeOut = 10 * time.Second
-
-var configuration *config.Config
-var Log logger.Logger
-
-func init() {
-	var err error
-	configuration, err = config.NewConfig("viper.config", "../../config")
-	if err != nil {
-		fmt.Println(err)
-	}
-	loggerConfig := configuration.NewLoggerConfig()
-	logUM := logrus.New()
-	err = logger.ConfigLogger(logUM, loggerConfig)
-	if err != nil {
-		fmt.Println(err)
-	}
-	logger.SetLogger(logUM)
-	Log = logger.LogUM
-}
 
 func main() {
 	var (
@@ -50,12 +30,34 @@ func main() {
 		wg          = new(sync.WaitGroup)
 		closers     []io.Closer
 	)
-	var err error
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	loggerConfig, err := cfg.LoggerConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = logger.SetLogger(loggerConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Example
+	log.Println(cfg)
+	log.Println(cfg.LoggerConfig(ctx))
+	log.Println(cfg.DBConfig(ctx))
 
 	// TODO: Replace with HTTP server implemented in server package
-	srv := &http.Server{Addr: ":8099"}
-	Log.Info("Server start at %s...", srv.Addr)
-	fmt.Println("A am started")
+	srv := &http.Server{
+		Addr:         cfg.ServerAddress(),
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
+	}
+
 	// Go routine with run HTTP server
 	wg.Add(1)
 
@@ -65,22 +67,24 @@ func main() {
 
 		err := srv.ListenAndServe()
 		if err != nil && err != http.ErrServerClosed {
-			Log.Error("%v\n", err)
+			logger.LogUM.Error("%v\n", err)
 		}
 	}()
-	Log.Info("Server Listening at %s...", srv.Addr)
+	logger.LogUM.Info("Server Listening at %s...", srv.Addr)
 
 	dbConfig, err := cfg.DBConfig(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	db, err := storage.ConnectToDB(dbConfig)
+
 	if err != nil {
 		logger.LogUM.Error("%v\n", err)
 		return
 	}
 
-	logger.LogUM.Info("Successfully connected to %s", dbConfig.DBName)
+	logger.LogUM.Infof("Successfully connected to %s", dbConfig.DBName)
 
 	closers = append(closers, db)
 
@@ -94,20 +98,20 @@ func main() {
 
 	select {
 	case <-interrupt:
-		Log.Info("Pressed Ctrl+C to terminate server...")
+		logger.LogUM.Info("Pressed Ctrl+C to terminate server...")
 		cancel()
 	case <-ctx.Done():
 		code = 1
 	}
 
-	Log.Info("Server is Stopping...")
+	logger.LogUM.Info("Server is Stopping...")
 
 	// Stop application
 	err = gracefulShutdown(gracefulShutdownTimeOut, wg, srv, closers...)
 	if err != nil {
-		Log.Panicf("Server graceful shutdown failed: %v", err)
+		logger.LogUM.Fatalf("Server graceful shutdown failed: %v", err)
 	}
 
-	Log.Info("Server was gracefully stopped!")
+	logger.LogUM.Info("Server was gracefully stopped!")
 	os.Exit(code)
 }
