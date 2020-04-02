4b1746b85b26f7d457003c1dc9b72e2764dd425c
Vasyl Kosiv <vasylkosiv@gmail.com>
2020-04-02 22:44:59 +0300

    add skiping integration test on github

--- storage/postgres_test.go
+++ storage/postgres_test.go
@@ -8,26 +8,30 @@ import (
 )
 
 const (
-	dbUser       = "POSTGRES_USER"
-	dbPassword   = "POSTGRES_PASSWORD"
-	db           = "POSTGRES_DB"
+	dbUser           = "POSTGRES_USER"
+	dbPassword       = "POSTGRES_PASSWORD"
+	db               = "POSTGRES_DB"
 	expectedLineOk   = "host=localhost port=5432 user=postgres password=postgres dbname=um_db sslmode=disable"
 	expectedLineZero = "host= port=0 user= password= dbname= sslmode=disable"
 )
 
 func TestConnectToDB(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
 	user, _ := os.LookupEnv(dbUser)
 	password, _ := os.LookupEnv(dbPassword)
 	dbname, _ := os.LookupEnv(db)
 
 	conf := &DBConfig{
-		Host:     "localhost",
+		Host:     "db",
 		Port:     5432,
 		User:     user,
 		Password: password,
 		DBName:   dbname,
 	}
-	
+
 	zeroConf := &DBConfig{}
 
 	bd, err := ConnectToDB(conf)
