61c81c96bb2e782a8c49d0691cd8bb0bb214bd25
Bohdan Volykh <wbodjaw@gmail.com>
2020-04-04 23:22:59 +0300

    change tests using mock generation

--- middleware/authentication_test.go
+++ middleware/authentication_test.go
@@ -1,4 +1,4 @@
-package middleware
+package middleware_test
 
 import (
 	"encoding/json"
@@ -8,9 +8,12 @@ import (
 	"os"
 	"testing"
 
-	"github.com/lvl484/user-manager/logger"
-	"github.com/lvl484/user-manager/model"
+	"github.com/lvl484/user-manager/middleware"
+	"github.com/lvl484/user-manager/mock"
 
+	gomock "github.com/golang/mock/gomock"
+	"github.com/lvl484/user-manager/logger"
+	model "github.com/lvl484/user-manager/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -24,8 +27,14 @@ func TestMain(m *testing.M) {
 }
 
 func TestBasicAuthenticationMiddlewareValidPass(t *testing.T) {
-	ma := &MockAuth{}
-	ba := NewBasicAuthentication(ma)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := mock.NewMockUserProvider(ctrl)
+
+	mock.EXPECT().GetInfo("i3odja").Return(returnedValue, nil)
+
+	ba := middleware.NewBasicAuthentication(mock)
 
 	r, err := http.NewRequest("GET", "/summer", nil)
 	require.NoError(t, err)
@@ -40,8 +49,14 @@ func TestBasicAuthenticationMiddlewareValidPass(t *testing.T) {
 }
 
 func TestBasicAuthenticationMiddlewareInvalidPass(t *testing.T) {
-	ma := &MockAuth{}
-	ba := NewBasicAuthentication(ma)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := mock.NewMockUserProvider(ctrl)
+
+	mock.EXPECT().GetInfo("i3odja").Return(returnedValue, nil)
+
+	ba := middleware.NewBasicAuthentication(mock)
 
 	r, err := http.NewRequest("GET", "/summer", nil)
 	require.NoError(t, err)
@@ -56,8 +71,14 @@ func TestBasicAuthenticationMiddlewareInvalidPass(t *testing.T) {
 }
 
 func TestBasicAuthenticationMiddlewareError(t *testing.T) {
-	ma := &MockAuth{Error: errors.New("middleware error")}
-	ba := NewBasicAuthentication(ma)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := mock.NewMockUserProvider(ctrl)
+
+	mock.EXPECT().GetInfo("i3odja").Return(returnedValue, errors.New("middleware error"))
+
+	ba := middleware.NewBasicAuthentication(mock)
 
 	r, err := http.NewRequest("GET", "/summer", nil)
 	require.NoError(t, err)
@@ -71,6 +92,28 @@ func TestBasicAuthenticationMiddlewareError(t *testing.T) {
 	checkErrorResponse(t, w, http.StatusInternalServerError)
 }
 
+func TestBasicAuthenticationMiddlewareResponseInvalid(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mock := mock.NewMockUserProvider(ctrl)
+
+	ba := middleware.NewBasicAuthentication(mock)
+
+	r, err := http.NewRequest("GET", "/summer", nil)
+	require.NoError(t, err)
+
+	w := httptest.NewRecorder()
+
+	ba.Middleware(OK).ServeHTTP(w, r)
+
+	checkErrorResponse(t, w, http.StatusUnauthorized)
+}
+
+var OK http.Handler = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+})
+
 func checkErrorResponse(t *testing.T, w *httptest.ResponseRecorder, expectedCode int) {
 	mError := model.Error{}
 
@@ -83,24 +126,14 @@ func checkErrorResponse(t *testing.T, w *httptest.ResponseRecorder, expectedCode
 	assert.Equal(t, expectedCode, w.Code)
 }
 
-type MockAuth struct {
-	Error error
+var returnedValue = &model.User{
+	ID:        "123e4567-e89b-12d3-a456-426655440000",
+	Username:  "i3odja",
+	Password:  "$argon2id$v=19$m=65536,t=3,p=1$Ga9X4EvymyOzUoz+uVMy6w$y5sQVQ",
+	Email:     "qwerty@gmail.com",
+	FirstName: "UserF",
+	LastName:  "UserL",
+	Phone:     0671112233,
+	CreatedAt: nil,
+	UpdatedAt: nil,
 }
-
-func (ma *MockAuth) GetInfo(username string) (*model.User, error) {
-	return &model.User{
-		ID:        "123e4567-e89b-12d3-a456-426655440000",
-		Username:  username,
-		Password:  "$argon2id$v=19$m=65536,t=3,p=1$Ga9X4EvymyOzUoz+uVMy6w$y5sQVQ",
-		Email:     "qwerty@gmail.com",
-		FirstName: "UserF",
-		LastName:  "UserL",
-		Phone:     0671112233,
-		CreatedAt: nil,
-		UpdatedAt: nil,
-	}, ma.Error
-}
-
-var OK http.Handler = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-	writer.WriteHeader(http.StatusOK)
-})
