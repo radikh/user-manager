916a43ec17299d8a7a9f4480d08367e59574dc33
Vasyl Kosiv <vasylkosiv@gmail.com>
2020-04-13 14:09:00 +0300

    Remove routers to the separate function

--- server/http.go
+++ server/http.go
@@ -4,8 +4,10 @@
 package server
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/lvl484/user-manager/config"
 	"github.com/lvl484/user-manager/logger"
 	"github.com/lvl484/user-manager/middleware"
 	"github.com/lvl484/user-manager/model"
@@ -15,29 +17,47 @@ import (
 
 // HTTP server struct
 type HTTP struct {
-	address string
-	acc     *account
-	ur      *model.UsersRepo
+	srv *http.Server
+	acc *account
+	ur  *model.UsersRepo
 }
 
 // NewHTTP get address of server and return pointer to newserver
-func NewHTTP(addr string, ur *model.UsersRepo) *HTTP {
-	return &HTTP{
-		address: addr,
-		ur:      ur,
+func NewHTTP(cfg *config.Config, ur *model.UsersRepo) *HTTP {
+	srv := &http.Server{
+		Addr:         cfg.ServerAddress(),
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
 	}
+
+	return &HTTP{
+		srv: srv,
+		ur:  ur,
+		acc: (*account)(ur),
+	}
+}
+func (h *HTTP) NewRouter() *mux.Router {
+	mainRoute := mux.NewRouter()
+	withoutAuth := mainRoute.PathPrefix("/account")
+	withoutAuth.HandlerFunc(h.acc.CreateAccount).Methods(http.MethodPost)
+
+	auth := mainRoute.PathPrefix("").Subrouter()
+	auth.Use(middleware.NewBasicAuthentication(h.ur).Middleware)
+	auth.HandleFunc("/account", h.acc.GetInfoAccount).Methods(http.MethodGet)
+	auth.HandleFunc("/account", h.acc.UpdateAccount).Methods(http.MethodPut)
+	auth.HandleFunc("/account", h.acc.DeleteAccount).Methods(http.MethodDelete)
+	auth.HandleFunc("/validate", h.acc.ValidateAccount).Methods(http.MethodGet)
+
+	return mainRoute
 }
 
 // Start create all routes and starting server
 func (h *HTTP) Start() error {
-	mainRoute := mux.NewRouter()
-	mainRoute.Use(middleware.NewBasicAuthentication(h.ur).Middleware)
-	mainRoute.HandleFunc("/account", h.acc.CreateAccount).Methods("POST")
-	mainRoute.HandleFunc("/account", h.acc.GetInfoAccount).Methods("GET")
-	mainRoute.HandleFunc("/account", h.acc.UpdateAccount).Methods("PUT")
-	mainRoute.HandleFunc("/account", h.acc.DeleteAccount).Methods("DELETE")
-	mainRoute.HandleFunc("/validate", h.acc.ValidateAccount).Methods("GET")
+	logger.LogUM.Infof("Server Listening at %s...", h.srv.Addr)
+	return http.ListenAndServe(h.srv.Addr, h.NewRouter())
+}
 
-	logger.LogUM.Infof("Server Listening at %s...", h.address)
-	return http.ListenAndServe(h.address, mainRoute)
+// Stop stops all routes and stopping server
+func (h *HTTP) Stop(ctx context.Context) error {
+	return h.srv.Shutdown(ctx)
 }
