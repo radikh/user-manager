20e4f8a5a7402462d34a0416653b4921c035716c
Ostap Klymchuk <oklymchuk@gmail.com>
2020-04-17 17:04:52 +0300

    Fix test

--- cmd/umcli/action_test.go
+++ cmd/umcli/action_test.go
@@ -18,6 +18,8 @@ func TestCreateAction(t *testing.T) {
 	defer mockCtrl.Finish()
 	cliMock := NewMockActionChecker(mockCtrl)
 	cliMock.EXPECT().ExecuteAction(context, actionCreate).Return(nil)
+	err = CreateAction(context)
+	assert.NoError(t, err)
 	err = cliMock.ExecuteAction(context, actionCreate)
 	assert.NoError(t, err)
 }
